9b4e9868d31ec353a8a71d8e5bb252e1bc4175a2
LeslieYQ <yuqiu-1988@163.com>
2015-12-03 21:59:56 +0800

    first commit

--- AutolayoutAnimation/ViewController.m
+++ AutolayoutAnimation/ViewController.m
@@ -7,21 +7,59 @@
 //
 
 #import "ViewController.h"
+#import "HomePageAnimationUtil.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *topTitleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *bottomTitleLabel;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *textFieldBottomLineConstraint;
+@property (weak, nonatomic) IBOutlet UIImageView *phoneIconImageView;
+@property (weak, nonatomic) IBOutlet UILabel *topTipsLabel;
+@property (weak, nonatomic) IBOutlet UIView *bottomTipsView;
 
 @end
 
 @implementation ViewController
 
+#pragma mark - Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    _topTitleLabel.transform = CGAffineTransformMakeTranslation(0, -200);
+    _bottomTitleLabel.transform = CGAffineTransformMakeTranslation(0, -200);
+    
+    _phoneIconImageView.transform = CGAffineTransformMakeTranslation(-200, 0);
+    
+    _textFieldBottomLineConstraint.constant = 0;
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [HomePageAnimationUtil titleLabelAnimationWithLabel:_topTitleLabel withView:self.view];
+    [HomePageAnimationUtil titleLabelAnimationWithLabel:_bottomTitleLabel withView:self.view];
+    
+    [HomePageAnimationUtil textFieldBottomLineAnimationWithConstraint:_textFieldBottomLineConstraint WithView:self.view];
+    
+    [HomePageAnimationUtil phoneIconAnimationWithLabel:_phoneIconImageView withView:self.view];
+    
+    [HomePageAnimationUtil tipsLabelMaskAnimation:_topTipsLabel withBeginTime:0];
+    [HomePageAnimationUtil tipsLabelMaskAnimation:_bottomTipsView withBeginTime:1];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - override view method
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
 }
 
 @end
